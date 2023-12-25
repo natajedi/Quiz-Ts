e9add5dd714fa42e1782f45801af71727f04de53
Natalya Prokhorova <105797271+natajedi@users.noreply.github.com>
2023-12-25 22:38:24 +0300

    Add fixes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import { QuizService } from './services/QuizService';
 import { UIService } from './services/UIService';
 import { QuestionModel } from './models/QuestionModel';
@@ -8,33 +7,41 @@ let session: QuizSessionModel = new QuizSessionModel();
 let question: QuestionModel | null = null;
 let showNextQuestion: boolean = false;
 
-document.getElementById("startButton")?.addEventListener("click", () => {
- session = QuizService.startNewQuiz();
- question = session.currentQuestion;
- UIService.updateQuestionText(question);
- UIService.displayPossibleAnswers(question);
-});
+fetch('./dbjson') 
+ .then(response => response.json())
+ .then(data => {
+ const quizService = new QuizService(data.questions);
 
-document.getElementById("nextButton")?.addEventListener("click", () => {
- session = QuizService.nextQuestion(session);
- question = session.currentQuestion;
- UIService.updateQuestionText(question);
- UIService.displayPossibleAnswers(question);
- showNextQuestion = false;
-});
-
-document.querySelectorAll(".answer").forEach((element: Element) => {
- element.addEventListener("click", (event: Event) => {
-  const answerElement = event.target as HTMLElement;
-  const answerId = parseInt(answerElement.dataset.id || "");
-  const result = QuizService.checkAnswer(session, answerId);
-  session = result.session;
-  question = result.question;
-  showNextQuestion = result.showNextQuestion;
-  UIService.updateQuestionText(question);
-  UIService.displayPossibleAnswers(question);
-  UIService.updateScore(session);
+ document.getElementById("startButton")?.addEventListener("click", () => {
+   session = quizService.startNewQuiz();
+   question = session.currentQuestion;
+   UIService.updateQuestionText(question);
+   UIService.displayPossibleAnswers(question);
  });
-});
+
+ document.getElementById("nextButton")?.addEventListener("click", () => {
+   session = quizService.nextQuestion();
+   question = session.currentQuestion;
+   UIService.updateQuestionText(question);
+   UIService.displayPossibleAnswers(question);
+   showNextQuestion = false;
+ });
+
+ document.querySelectorAll(".answer").forEach((element: Element) => {
+   element.addEventListener("click", (event: Event) => {
+     const answerElement = event.target as HTMLElement;
+     const answerId = parseInt(answerElement.dataset.id || "");
+     const result = quizService.checkAnswer(session, answerId);
+     session = result.session;
+     question = result.question;
+     showNextQuestion = result.showNextQuestion;
+     UIService.updateQuestionText(question);
+     UIService.displayPossibleAnswers(question);
+     UIService.updateScore(session);
+   });
+ });
+ })
+ .catch(error => console.error('Error:', error));
+
 
 
