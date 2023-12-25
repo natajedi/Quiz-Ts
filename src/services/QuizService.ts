f6d49860e0518ffd8e19ac58539d491cc8eda56f
Natalya Prokhorova <105797271+natajedi@users.noreply.github.com>
2023-12-25 22:42:54 +0300

    Added fixes

--- src/services/QuizService.ts
+++ src/services/QuizService.ts
@@ -3,43 +3,49 @@ import { IUserSelectedAnswer } from '../models/AnswerModel';
 import { QuizSessionModel } from '../models/QuizSessionModel';
 
 export class QuizService {
- questions: IQuestion[];
- currentQuestionIndex: number;
- selectedAnswerIds: number[];
- score: number;
- currentQuestion: IQuestion | null;
+ private questions: IQuestion[];
+ private currentQuestionIndex: number;
+ private selectedAnswerIds: number[];
+ private score: number;
+ private currentQuestion: IQuestion | null;
 
  constructor(questions: IQuestion[]) {
-   this.questions = questions;
-   this.currentQuestionIndex = 0;
-   this.selectedAnswerIds = [];
-   this.score = 0;
-   this.currentQuestion = this.questions[this.currentQuestionIndex];
+ this.questions = questions;
+ this.currentQuestionIndex = 0;
+ this.selectedAnswerIds = [];
+ this.score = 0;
+ this.currentQuestion = this.questions[this.currentQuestionIndex];
  }
 
  startNewQuiz() {
-   this.currentQuestionIndex = 0;
-   this.selectedAnswerIds = [];
-   this.score = 0;
-   this.currentQuestion = this.questions[this.currentQuestionIndex];
-   return new QuizSessionModel(this.currentQuestionIndex, this.selectedAnswerIds, this.score, this.currentQuestion);
+ this.currentQuestionIndex = 0;
+ this.selectedAnswerIds = [];
+ this.score = 0;
+ this.currentQuestion = this.questions[this.currentQuestionIndex];
+ return new QuizSessionModel(this.currentQuestionIndex, this.selectedAnswerIds, this.score, this.currentQuestion);
  }
 
  nextQuestion() {
-   this.currentQuestionIndex++;
-   this.currentQuestion = this.questions[this.currentQuestionIndex];
-   return new QuizSessionModel(this.currentQuestionIndex, this.selectedAnswerIds, this.score, this.currentQuestion);
+ this.currentQuestionIndex++;
+ this.currentQuestion = this.questions[this.currentQuestionIndex];
+ return new QuizSessionModel(this.currentQuestionIndex, this.selectedAnswerIds, this.score, this.currentQuestion);
  }
 
  checkAnswer(session: QuizSessionModel, answerId: number) {
-   const isCorrect = session.currentQuestion.answers.some(answer => answer.id === answerId && answer.correct);
-   const correctAnswerId = session.currentQuestion.answers.find(answer => answer.correct)?.id || -1;
-   return { isCorrect, correctAnswerId, session: new QuizSessionModel(this.currentQuestionIndex, this.selectedAnswerIds, this.score, this.currentQuestion) };
+ if (!session) {
+  throw new Error('Session is null');
+ }
+ if (!session.currentQuestion) {
+  throw new Error('Current question is null');
+ }
+ const isCorrect = session.currentQuestion.answers.some(answer => answer.id === answerId && answer.correct);
+ const correctAnswerId = session.currentQuestion.answers.find(answer => answer.correct)?.id || -1;
+ return { isCorrect, correctAnswerId, session: new QuizSessionModel(this.currentQuestionIndex, this.selectedAnswerIds, this.score, this.currentQuestion) };
  }
 
  calculateResult(answers: IUserSelectedAnswer[]) {
-   const correctAnswersCount = answers.reduce((count, answer) => count + (this.questions.find(question => question.id === answer.questionId)?.answers.find(a => a.id === answer.answerId)?.correct ? 1 : 0), 0);
-   return { score: correctAnswersCount };
+ const correctAnswersCount = answers.reduce((count, answer) => count + (this.questions.find(question => question.id === answer.questionId)?.answers.find(a => a.id === answer.answerId)?.correct ? 1 : 0), 0);
+ return { score: correctAnswersCount };
  }
 }
 
--- src/services/UIService.ts
+++ src/services/UIService.ts
@@ -1,37 +1,77 @@
-import { IQuestion } from './QuestionModel';
-import { IUserSelectedAnswer } from './AnswerModel';
+import { IAnswer, IQuestion } from '../models/QuestionModel';
+/*import { IUserSelected } from '../models/AnswerModel';*/
+import { QuizSessionModel } from '../models/QuizSessionModel';
 
 export class UIService {
+ static updateQuestionText(question: import("../models/QuestionModel").QuestionModel | null) {
+   throw new Error('Method not implemented.');
+ }
+ static displayPossibleAnswers(question: import("../models/QuestionModel").QuestionModel | null) {
+   throw new Error('Method not implemented.');
+ }
+ static updateScore(session: QuizSessionModel) {
+   throw new Error('Method not implemented.');
+ }
  updateQuestionText(question: IQuestion | null) {
-  const questionElement = document.querySelector<HTMLHeadingElement>('#question-text') as HTMLHeadingElement;
-  if (question) {
-    questionElement.innerHTML = `${question.id}. ${question.text}`;
-  } else {
-    questionElement.innerHTML = '';
-  }
+ const questionElement = document.querySelector<HTMLHeadingElement>('#question-text') as HTMLHeadingElement;
+ if (question) {
+  questionElement.innerHTML = `${question.id}. ${question.text}`;
+ } else {
+  questionElement.innerHTML = '';
+ }
  }
 
  displayPossibleAnswers(question: IQuestion | null) {
-  const answerButtonsContainer = document.querySelector<HTMLDivElement>('#answer-buttons') as HTMLDivElement;
-  if (question) {
-    question.answers.forEach(answer => {
-      const button = document.createElement('button');
-      button.innerHTML = answer.text;
-      button.type = 'button';
-      button.classList.add('button', 'question__answer');
-      button.dataset.id = answer.id.toString();
-      button.addEventListener('click', selectAnswer);
-      answerButtonsContainer.appendChild(button);
-    });
-  } else {
-    while (answerButtonsContainer.firstChild) {
-      answerButtonsContainer.removeChild(answerButtonsContainer.firstChild);
-    }
+ const answerButtonsContainer = document.querySelector<HTMLDivElement>('#answer-buttons') as HTMLDivElement;
+ if (question) {
+  question.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerHTML = answer.text;
+    button.type = 'button';
+    button.classList.add('button', 'question__answer');
+    button.dataset.questionId = question.id.toString();
+    button.addEventListener('click', this.selectAnswer);
+    answerButtonsContainer.appendChild(button);
+  });
+ } else {
+  while (answerButtonsContainer.firstChild) {
+    answerButtonsContainer.removeChild(answerButtonsContainer.firstChild);
   }
+ }
  }
 
  updateScore(session: QuizSessionModel) {
-  const scoreElement = document.querySelector<HTMLParagraphElement>('#score') as HTMLParagraphElement;
-  scoreElement.innerHTML = `Your score: ${session.score}`;
+ const scoreElement = document.querySelector<HTMLParagraphElement>('#score') as HTMLParagraphElement;
+ scoreElement.innerHTML = `Your score: ${session.score}`;
  }
-}
+
+ public async selectAnswer(event: Event) {
+  const target = event.target as HTMLButtonElement;
+  const answerId = Number(target.dataset.id);
+  const questionId = Number(target.dataset.questionId);
+ 
+  // Получить все вопросы из db.json
+  const response = await fetch('./db.json');
+  const data: { questions: IQuestion[] } = await response.json();
+  const questions = data.questions;
+ 
+  // Найти вопрос по ID
+  const question = questions.find((q: IQuestion) => q.id === questionId);
+  if (!question) {
+  throw new Error(`Question with id ${questionId} not found`);
+  }
+ 
+  // Найти правильный ответ для этого вопроса
+  const correctAnswer = question.answers.find((a: IAnswer) => a.correct);
+  if (!correctAnswer) {
+  throw new Error(`No correct answer found for question ${questionId}`);
+  }
+ 
+  const correctAnswerId = correctAnswer.id;
+  if (answerId === correctAnswerId) {
+  console.log('Пользователь ответил правильно!');
+  } else {
+  console.log('Пользователь ответил неправильно. Правильный ответ: ', correctAnswerId);
+  }
+ }
+} 
